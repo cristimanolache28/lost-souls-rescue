e8e49c08df20293564f137bf2cf09bc5f4ff7c03
Cristian Manolache <cristianmanolache0@gmail.com>
2024-03-08 18:17:48 +0200

    Initial commit

---
+++ src/main/java/com/lsr/LostSoulsRescueApplication.java
@@ -0,0 +1,13 @@
+package com.lsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostSoulsRescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostSoulsRescueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsr/LostSoulsRescueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostSoulsRescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
